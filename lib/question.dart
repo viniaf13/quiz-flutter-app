1b68ae17e4478764c52e1e759e80fb4f5c725af2
Vinicius Aguiar <viniaf13@gmail.com>
2021-03-02 16:54:48 -0300

    Update question.dart

--- lib/question.dart
+++ lib/question.dart
@@ -2,8 +2,10 @@ class Question {
   String text;
   bool answer;
 
-  Question(text, answer) {
+  /*Question(text, answer) {
     this.text = text;
     this.answer = answer;
-  }
+  }*/
+
+  Question(this.text, this.answer);
 }
